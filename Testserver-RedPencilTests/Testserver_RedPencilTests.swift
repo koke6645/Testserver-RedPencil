b1adca7f531435d6cda48bdd009c21a11a87120f
koke6645 <koke6645@gmail.com>
2016-02-11 14:09:32 -0600

    Initial Commit

---
+++ Testserver-RedPencilTests/Testserver_RedPencilTests.swift
@@ -0,0 +1,36 @@
+//
+//  Testserver_RedPencilTests.swift
+//  Testserver-RedPencilTests
+//
+//  Created by Red_iMac on 2/11/16.
+//  Copyright © 2016 RedPencil. All rights reserved.
+//
+
+import XCTest
+@testable import Testserver_RedPencil
+
+class Testserver_RedPencilTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
